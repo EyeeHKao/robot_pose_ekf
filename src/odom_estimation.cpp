3f114ac8febe5f71edd00c7ab57d4aec62f4eb0d
EyeeH <990457372@qq.com>
2020-03-05 15:31:59 +0800

    Update odom_estimation.cpp

--- src/odom_estimation.cpp
+++ src/odom_estimation.cpp
@@ -65,27 +65,27 @@ namespace estimation
     sys_pdf_   = new NonLinearAnalyticConditionalGaussianOdo(system_Uncertainty);
     sys_model_ = new AnalyticSystemModelGaussianUncertainty(sys_pdf_);
 
-    // create MEASUREMENT MODEL ODOM
+    // create MEASUREMENT MODEL ODOM:观测值只提供x,y,yaw
     ColumnVector measNoiseOdom_Mu(6);  measNoiseOdom_Mu = 0;
     SymmetricMatrix measNoiseOdom_Cov(6);  measNoiseOdom_Cov = 0;
     for (unsigned int i=1; i<=6; i++) measNoiseOdom_Cov(i,i) = 1;
     Gaussian measurement_Uncertainty_Odom(measNoiseOdom_Mu, measNoiseOdom_Cov);
     Matrix Hodom(6,6);  Hodom = 0;
-    Hodom(1,1) = 1;    Hodom(2,2) = 1;    Hodom(6,6) = 1;
+    Hodom(1,1) = 1;    Hodom(2,2) = 1;    Hodom(6,6) = 1;	///轮式里程计更新了状态中的x,y,yaw，z_odom = H_66 * x_61
     odom_meas_pdf_   = new LinearAnalyticConditionalGaussian(Hodom, measurement_Uncertainty_Odom);
     odom_meas_model_ = new LinearAnalyticMeasurementModelGaussianUncertainty(odom_meas_pdf_);
 
-    // create MEASUREMENT MODEL IMU
+    // create MEASUREMENT MODEL IMU：观测值只提供r,p,y
     ColumnVector measNoiseImu_Mu(3);  measNoiseImu_Mu = 0;
     SymmetricMatrix measNoiseImu_Cov(3);  measNoiseImu_Cov = 0;
     for (unsigned int i=1; i<=3; i++) measNoiseImu_Cov(i,i) = 1;
     Gaussian measurement_Uncertainty_Imu(measNoiseImu_Mu, measNoiseImu_Cov);
     Matrix Himu(3,6);  Himu = 0;
-    Himu(1,4) = 1;    Himu(2,5) = 1;    Himu(3,6) = 1;
+    Himu(1,4) = 1;    Himu(2,5) = 1;    Himu(3,6) = 1;	///imu观测值更新了r,p,y z_imu= H_36 * x_61
     imu_meas_pdf_   = new LinearAnalyticConditionalGaussian(Himu, measurement_Uncertainty_Imu);
     imu_meas_model_ = new LinearAnalyticMeasurementModelGaussianUncertainty(imu_meas_pdf_);
 
-    // create MEASUREMENT MODEL VO
+    // create MEASUREMENT MODEL VO:观测值提供了x,y,z,r,p,y
     ColumnVector measNoiseVo_Mu(6);  measNoiseVo_Mu = 0;
     SymmetricMatrix measNoiseVo_Cov(6);  measNoiseVo_Cov = 0;
     for (unsigned int i=1; i<=6; i++) measNoiseVo_Cov(i,i) = 1;
@@ -95,7 +95,7 @@ namespace estimation
     vo_meas_pdf_   = new LinearAnalyticConditionalGaussian(Hvo, measurement_Uncertainty_Vo);
     vo_meas_model_ = new LinearAnalyticMeasurementModelGaussianUncertainty(vo_meas_pdf_);
 
-    // create MEASUREMENT MODEL GPS
+    // create MEASUREMENT MODEL GPS：观测值只提供x,y,z
     ColumnVector measNoiseGps_Mu(3);  measNoiseGps_Mu = 0;
     SymmetricMatrix measNoiseGps_Cov(3);  measNoiseGps_Cov = 0;
     for (unsigned int i=1; i<=3; i++) measNoiseGps_Cov(i,i) = 1;
@@ -142,6 +142,7 @@ namespace estimation
     filter_ = new ExtendedKalmanFilter(prior_);
 
     // remember prior
+    //添加到tf tree中， output_frame_为父坐标系（惯性系，world），base_footprint_frame_（随机器人运动的，机器中心在地面上的投影）为子坐标系，这个transform表示子系到父系的坐标变换（或子系在父系中的位姿）  
     addMeasurement(StampedTransform(prior, time, output_frame_, base_footprint_frame_));
     filter_estimate_old_vec_ = prior_Mu;
     filter_estimate_old_ = prior;
@@ -164,9 +165,9 @@ namespace estimation
       return false;
     }
 
-    // only update filter for time later than current filter time
+    // only update filter for time later than current filter time：
     double dt = (filter_time - filter_time_old_).toSec();
-    if (dt == 0) return false;
+    if (dt == 0) return false;	///如果滤波器的更新频率大于传感器数据的产生频率，那么会产生这种情形
     if (dt <  0){
       ROS_INFO("Will not update robot pose with time %f sec in the past.", dt);
       return false;
@@ -176,19 +177,21 @@ namespace estimation
 
     // system update filter
     // --------------------
-    // for now only add system noise
+    // for now only add system noise:预测，这里直接在上次状态上加了过程噪声，预测状态均值等于上次状态均值
     ColumnVector vel_desi(2); vel_desi = 0;
     filter_->Update(sys_model_, vel_desi);
 
-    
+    //根据各传感器此时是否激活，将传感器测量值对齐到当前filer_time上来，统一做更新，相当于各个传感器根据各自噪声来加权更新系统状态
     // process odom measurement
     // ------------------------
     ROS_DEBUG("Process odom meas");
     if (odom_active){
+	    //canTransform("target_frame", "source_frame", time):查询是否可获取source_frame到target_frame的相对位姿
       if (!transformer_.canTransform(base_footprint_frame_,"wheelodom", filter_time)){
-        ROS_ERROR("filter time older than odom message buffer");
+        ROS_ERROR("filter time older than odom message buffer");	///通过tf tree查询是否可以获取filter_time时刻轮速里程计
         return false;
       }
+	    //lookupTransform("target_frame", "source_frame", time, res):获取从source_frame到target_frame的相对位姿
       transformer_.lookupTransform("wheelodom", base_footprint_frame_, filter_time, odom_meas_);
       if (odom_initialized_){
 	// convert absolute odom measurements to relative odom measurements in horizontal plane
